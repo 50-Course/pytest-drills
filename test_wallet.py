1fd1c85febfd2ea6b2cf9aa4297801f7753ad8fe
Eri Adeodu <sinmiwaye2001@gmail.com>
2022-09-16 17:05:14 +0000

    Working with Parameterized values

--- test_wallet.py
+++ test_wallet.py
@@ -5,31 +5,56 @@
 #    * Test User can add cash
 #    * Users can spend cash
 #     * An Error is raised if the user has insufficient wallet balance
+#
+# Test parametrization:
+#    
 
 import pytest
 from wallet.wallet import Wallet
 from wallet.exceptions import InsufficientAmountException
 
 
-def test_default_initial_balance():
-    wallet = Wallet()
-    assert wallet.balance == 0
+# --- FIXTURES ---
+@pytest.fixture
+def empty_wallet():
+    """ Returns a wallet with zero balance. """
+    return Wallet()
 
-def test_default_setting_initial_balance():
-    wallet = Wallet(100)
-    assert wallet.balance == 100
+@pytest.fixture
+def wallet():
+    """ Returns a wallet with 20$ balance. """
+    return Wallet(20)
 
-def test_user_can_add_cash():
-    wallet = Wallet(100)
-    wallet.add_cash(10)
-    assert wallet.balance == 110
+def test_default_initial_balance(empty_wallet):
+    assert empty_wallet.balance == 0
 
-def test_wallet_spend_cash():
-    wallet = Wallet(30)
-    wallet.spend_cash(10)
+
+def test_default_setting_initial_balance(wallet):
     assert wallet.balance == 20
 
-def test_wallet_raises_insufficient_amount_exception_on_low_balance():
-    wallet = Wallet()
+def test_user_can_add_cash(wallet):
+    wallet.add_cash(10)
+    assert wallet.balance == 30
+
+def test_wallet_spend_cash(wallet):
+    wallet.spend_cash(10)
+    assert wallet.balance == 10
+
+def test_wallet_raises_insufficient_amount_exception_on_low_balance(empty_wallet):
     with pytest.raises(InsufficientAmountException):
-        wallet.spend_cash(90)
+        empty_wallet.spend_cash(90)
+
+
+@pytest.mark.parametrize(
+    'earned, spent, expected',
+    [
+        (40, 30, 10),
+        (9, 5, 4),
+        (10, 3, 7)
+    ]
+)
+def test_transactions(earned, spent, expected):
+    wallet = Wallet()
+    wallet.add_cash(earned)
+    wallet.spend_cash(spent)
+    assert wallet.balance == expected
